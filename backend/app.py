867f790a5c8828a4e53f2e2b285f2566cd570f3b
Shrey Nagda <shreynagda2714@gmail.com>
2024-06-03 17:23:06 +0530

    Using waitress for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,15 @@
 import random
 from flask import Flask, json, jsonify,request
 
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    print("Server running successfully!")
-    return jsonify("Server running successfully!")
+def create_app():
+    flask_app = Flask(__name__)
+    from routes import main_bp
+    flask_app.register_blueprint(main_bp)
+    return flask_app
 
-#Random Words
-@app.route("/words", methods=["GET"])
-def getWords():
-    n = int(request.args["n"]) or 5
-    with open('objects.txt', "r", encoding='utf-8') as file:
-        words_list = [line.strip() for line in file]
-    random_words = random.sample(words_list, n)
-    return jsonify(random_words)
 
-#Random Movies
-@app.route("/movies",methods=['GET'])
-def getMovies():
-    n = int(request.args["n"]) or 5
-    with open("movies.txt", "r", encoding="utf-8") as file:
-        movies_list = [line.strip() for line in file]
-    random_movies = random.sample(movies_list, n)
-    return jsonify(random_movies)
-
-@app.route("/cricketers")
-def getCricketers():
-    n = int(request.args["n"]) or 5
-    with open('cricketers.txt', "r", encoding='utf-8') as file:
-        cricketersList = [line.strip() for line in file]
-    random_cricketers = random.sample(cricketersList, n)
-    return jsonify(random_cricketers)
 
 if (__name__ == "__main__"):
+    app = create_app()
     app.run()
---
+++ backend/routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint,jsonify,request
+import random
+main_bp = Blueprint("main", __name__)
+@main_bp.route("/")
+def home():
+    print("Server running successfully!")
+    return jsonify("Server running successfully!")
+
+#Random Words
+@main_bp.route("/words", methods=["GET"])
+def getWords():
+    n = int(request.args["n"]) or 5
+    with open('objects.txt', "r", encoding='utf-8') as file:
+        words_list = [line.strip() for line in file]
+    random_words = random.sample(words_list, n)
+    return jsonify(random_words)
+
+#Random Movies
+@main_bp.route("/movies",methods=['GET'])
+def getMovies():
+    n = int(request.args["n"]) or 5
+    with open("movies.txt", "r", encoding="utf-8") as file:
+        movies_list = [line.strip() for line in file]
+    random_movies = random.sample(movies_list, n)
+    return jsonify(random_movies)
+
+@main_bp.route("/cricketers")
+def getCricketers():
+    n = int(request.args["n"]) or 5
+    with open('cricketers.txt', "r", encoding='utf-8') as file:
+        cricketersList = [line.strip() for line in file]
+    random_cricketers = random.sample(cricketersList, n)
+    return jsonify(random_cricketers)
