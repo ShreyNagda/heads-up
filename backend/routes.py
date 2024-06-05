d429106b95051e3903654e5eb8f1a9495f274d84
Shrey Nagda <shreynagda2714@gmail.com>
2024-06-05 11:50:48 +0530

    Response format changed & checking for arguments in the search query added

--- backend/routes.py
+++ backend/routes.py
@@ -9,34 +9,37 @@ def home():
 #Random Words
 @main_bp.route("/words", methods=["GET"])
 def getWords():
-    if(request.args != None):
+    print(request.args)
+    if(request.args.__contains__("n")):
         n = int(request.args["n"])
     else:
         n = 5
     with open('objects.txt', "r", encoding='utf-8') as file:
         words_list = [line.strip() for line in file]
     random_words = random.sample(words_list, n)
-    return jsonify(random_words)
+    return jsonify({"type": "Words", "value": random_words}), 200
 
 #Random Movies
 @main_bp.route("/movies",methods=['GET'])
 def getMovies():
-    if(request.args != None):
+    if(request.args.__contains__("n")):
         n = int(request.args["n"])
     else:
         n = 5
     with open("movies.txt", "r", encoding="utf-8") as file:
         movies_list = [line.strip() for line in file]
     random_movies = random.sample(movies_list, n)
-    return jsonify(random_movies)
+    return jsonify({"type": "Movies", "value": random_movies}), 200
 
+
+#IPL Cricketers
 @main_bp.route("/cricketers")
 def getCricketers():
-    if(request.args != None):
+    if(request.args.__contains__("n")):
         n = int(request.args["n"])
     else:
         n = 5
     with open('cricketers.txt', "r", encoding='utf-8') as file:
         cricketersList = [line.strip() for line in file]
     random_cricketers = random.sample(cricketersList, n)
-    return jsonify(random_cricketers)
+    return jsonify({"type": "Cricketers", "value": random_cricketers}), 200
